221d3f5e44b515be88448e1355fc9961bd98df84
Steve Szabo <steve.g.szabo@gmail.com>
2021-07-02 19:56:08 -0400

    updates

---
+++ skaffold/hashi-demo/tls/ingress.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HASHI_HOST=hashi-demo.example.com
+HASHI_CERT=example.com.crt
+
+while true
+do
+	curl -s -HHost:$HASHI_HOST --resolve $HASHI_HOST:8443:127.0.0.1 --cacert $HASHI_CERT https://$HASHI_HOST:8443/
+	date
+	sleep 1
+done
