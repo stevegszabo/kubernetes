a79b978dc72a42bc4550d2eb1c67acfd51ecbb0c
Steve Szabo <steve.g.szabo@gmail.com>
2021-09-30 16:32:19 -0400

    updates

--- docker/root/app/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WEBAPP_BIND="0.0.0.0:8080"
-WEBAPP_APP="app.webapp:GAPPLICATION"
-WEBAPP_LEVEL="debug"
-
-gunicorn3 --bind $WEBAPP_BIND $WEBAPP_APP --log-level $WEBAPP_LEVEL --access-logfile - --error-logfile -
-
-exit $?
